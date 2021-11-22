54b67ad526a1beadf30cfcf43ade85b1fe16e885
Cameron Moodley <moodley.cameron@gmail.com>
2021-11-22 12:10:06 +0100

    computed and custom methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import * as Vue from 'vue/dist/vue.esm-bundler.js';
 
 const app = Vue.createApp({
-	template: `<h1>Hello {{ msg }}</h1>`,
+	template: `
+    <p>{{count}}</p>
+    <div v-if="isEven(count)">Even</div>
+    <div v-else>Odd</div>
+
+    <div v-for="number in numbers">
+      <div :class="getClass(number)">
+        {{number}}
+      </div>
+    </div>
+    <br /><br />
+    <button @click="increment">Increment</button>
+  `,
 	data() {
 		return {
-			msg: 'Test',
+			count: 0,
+			numbers: [0, 1, 2, 3, 4, 5, 7, 8, 9, 10],
 		};
+	},
+	computed: {
+		evenList() {
+			return this.numbers.filter((num) => this.isEven(num));
+		},
+	},
+	methods: {
+		increment() {
+			this.count++;
+		},
+		isEven(val) {
+			return val % 2 === 0;
+		},
+		getClass(number) {
+			return this.isEven(number) ? 'blue' : 'red';
+		},
 	},
 });
 app.mount('#app');
+
+// all computed properties takes no arguments
+// Reactive numbers array changes the dom updates.
+// for business logic
+// dont user tenary inside HTML create a method for it
